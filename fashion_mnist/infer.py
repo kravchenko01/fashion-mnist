ce5fa25ccdc7aac13bfa41c47914deae5e92a816
Kravchenko01 <roma.kravchenco.2001@mail.ru>
2023-10-06 16:34:13 +0300

    Make train and infer modules for FascionCNN model

--- fashion_mnist/infer.py
+++ fashion_mnist/infer.py
@@ -1,7 +1,82 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+from torchvision import transforms, datasets
+
+from model import FashionCNN
 
 
+BATCH_SIZE = 100
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-if __name__ == 'main':
+def output_label(label):
+    output_mapping = {
+                 0: "T-shirt/Top",
+                 1: "Trouser",
+                 2: "Pullover",
+                 3: "Dress",
+                 4: "Coat", 
+                 5: "Sandal", 
+                 6: "Shirt",
+                 7: "Sneaker",
+                 8: "Bag",
+                 9: "Ankle Boot"
+                 }
+    input = (label.item() if type(label) == torch.Tensor else label)
+    return output_mapping[input]
+
+def test_model(model, test_loader):
+    # Testing the model   
+    model.eval()
+
+    correct = 0
+    total = 0
+
+    class_correct = [0. for _ in range(10)]
+    total_correct = [0. for _ in range(10)]
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            # test = Variable(images)
+            outputs = model(images)
+
+            predicted = torch.max(outputs, 1)[1].to(device)
+            c = (predicted == labels).squeeze()
+
+            correct += (predicted == labels).sum()
+            total += len(labels)
+            
+            for i in range(BATCH_SIZE):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                total_correct[label] += 1
+            
+
+    metrics_dct = {}
+    metrics_dct['Accuracy'] = (correct / total).item()
+
+    for i in range(10):
+        metrics_dct['Accuracy of {}:'.format(output_label(i))] = class_correct[i] / total_correct[i]
+    
+    return metrics_dct
+
+if __name__ == '__main__':
     # считываем с диска модель, загружаем валидационный датасет,
     # предсказываем моделью ответы для этих данных, записываем ответы на диск в .csv файл,
     # выводим в stdout (`print`) необходимые метрики на этом датасете.
+
+    test_set = datasets.FashionMNIST(root='./', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+    test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+
+    model = FashionCNN()
+    model.load_state_dict(torch.load('./trained_weights_FascionCNN.pth'))
+    model.to(device)
+
+    metrics_dct = test_model(model, test_loader)
+    print('METRICS ON TEST DATA:')
+    for i in metrics_dct:
+        print(i, metrics_dct[i])
+
+    
--- fashion_mnist/train.py
+++ fashion_mnist/train.py
@@ -1,5 +1,76 @@
+import matplotlib.pyplot as plt
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+from torchvision import transforms, datasets
+
+from model import FashionCNN
+
+
+BATCH_SIZE = 100
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train_model(model, train_loader):
+    model.train()
+
+    Loss_func = nn.CrossEntropyLoss()
+    learning_rate = 0.001
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    num_epochs = 5
+    count = 0
+
+    loss_list = []
+    iteration_list = []
+
+    for epoch in range(num_epochs):
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+        
+            # train = Variable(images.view(100, 1, 28, 28))
+            # labels = Variable(labels)
+             
+            outputs = model(images)
+            loss = Loss_func(outputs, labels)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+            count += 1
+
+            if not (count % 50):
+                loss_list.append(loss.data)
+                iteration_list.append(count)
+
+            if not (count % 500):
+                print("Iteration: {}, Loss: {}".format(count, loss.data))
+
+    return loss_list, iteration_list
 
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     #Загружаем данные, обучаем модель и сохраняем на диск
+
+    train_set = datasets.FashionMNIST(root='./', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+
+    # image, label = next(iter(train_set))
+    # print(label, image.shape)
+    # plt.imshow(image.squeeze(), cmap="gray")
+    # plt.savefig("./example.png")
+
+    model = FashionCNN()
+    model.to(device)
+
+    loss_list, iteration_list = train_model(model, train_loader)
+
+    torch.save(model.state_dict(), './trained_weights_FascionCNN.pth')
+
+    # plt.plot(iteration_list, loss_list)
+    # plt.xlabel("No. of Iteration")
+    # plt.ylabel("Loss")
+    # plt.title("Iterations vs Loss")
+    # plt.savefig("./training_loss.png")
